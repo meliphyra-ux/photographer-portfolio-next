9deeed53ad5bb94cb4f47d1c44b4c3e62e1ab1f2
meliphyra-ux <xennerit@icloud.com>
2022-10-01 18:25:06 +0300

    Responsivness improvement

--- pages/index.js
+++ pages/index.js
@@ -26,18 +26,18 @@ export default function index() {
 
   return (
     <section className="flex flex-row items-center sm:text-left justify-center sm:justify-between h-screen ">
-      <article className="lg:mx-32 mx-16 flex flex-col justify-evenly h-1/2">
-        <h1 className="text-white lg:text-6xl text-2xl mb-3">
+      <article className="lg:mx-32 mx-16 w-screen md:w-3/5 lg:w-3/6 flex flex-col justify-evenly h-1/2">
+        <h1 className="text-white lg:text-6xl text-2xl mb-6">
           Hello, I'm Sasha.
           <br />
           Professional
           <br />
           Photographer
         </h1>
-        <p className="text-white lg:text-4xl text-2xl font-extralight mb-3">
+        <p className="text-white lg:text-4xl text-2xl font-extralight mb-10">
           Lorem ipsum
         </p>
-        <button className="px-2.5 w-1/2 py-2 text-white border-2 border-white hover:bg-white hover:text-black duration-300 mb-3"
+        <button className="px-2.5 w-1/2 py-2 text-white border-2 border-white hover:bg-white hover:text-black duration-300 mb-4"
         onClick={() => router.push('/collections')}
         >
           Collections
@@ -48,7 +48,7 @@ export default function index() {
           <button className="mr-5"><a href="" target="_blank"><Image src={facebook} alt="Facebook" width={32} height={32}/></a></button>
         </section>
       </article>
-      <div className="overflow-hidden lg:w-2/5 md:flex hidden flex-row h-screen">
+      <div className="overflow-hidden md:w-2/5 lg:w-3/6 md:flex hidden flex-row h-screen">
         {photos.map((photo) => (
           <img
             src={photo.src}
