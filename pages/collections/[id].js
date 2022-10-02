150cdaf931e86ed8258e972b36cdbbbf7b02504a
meliphyra-ux <xennerit@icloud.com>
2022-10-02 19:36:10 +0300

    Small clean up of code

--- pages/collections/[id].js
+++ pages/collections/[id].js
@@ -1,17 +1,10 @@
 import { useRouter } from 'next/router'
-import { useState } from 'react'
 import { useSelector } from 'react-redux'
 
 const Collection = () => {
   const router = useRouter()
   const {id} = router.query
   const [collection] = useSelector(state => state.collection.value.filter(collection => collection.collectionName === id))
-  // const [collection, setCollection] = useState(null)
-
-  // useState(() => {
-  //   console.log(collectionGrabber)
-  //   setCollection(collectionGrabber[0])
-  // }, [collectionGrabber])
   return (
     <>
       {collection && <div>{collection.collectionName}</div>}
