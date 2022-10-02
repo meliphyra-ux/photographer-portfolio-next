712ddf53620e9c4ada985f728fad2104a34233bf
meliphyra-ux <xennerit@icloud.com>
2022-10-03 01:05:16 +0300

    Added photo display & little styles

--- pages/collections/[id].js
+++ pages/collections/[id].js
@@ -4,11 +4,21 @@ import { useSelector } from 'react-redux'
 const Collection = () => {
   const router = useRouter()
   const {id} = router.query
-  const [collection] = useSelector(state => state.collection.value.filter(collection => collection.collectionName === id))
+  const collection = useSelector(state => state.photos.photos).filter(collection => collection.collection === id)
+  
   return (
-    <>
-      {collection && <div>{collection.collectionName}</div>}
-    </>
+    <section>
+      {collection && 
+      <div>
+        <h1>{collection.collectionName}</h1>
+        {collection.map(photo => (
+          <figure key={photo.id} className="text-white w-72 h-72 relative">
+            <img src={photo.src} className="w-full h-full object-cover object-center hover:opacity-0.5 duration-150 "/>
+            <h3 className='w-full h-full absolute top-0 left-0'>{photo.description}</h3>
+          </figure>
+        ))}
+      </div>}
+    </section>
   )
 }
 
