4d0e966bf20ea54f40e81d567fe60fb9fefe1978
meliphyra-ux <xennerit@icloud.com>
2022-10-05 00:58:05 +0300

    Some changes in design

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,9 @@
 import React, { useEffect, useState } from "react";
 import instagram from "../styles/assets/instagram.png"
-import facebook from "../styles/assets/facebook.png"
 import telegram from "../styles/assets/telegram.png"
 import { useSelector } from "react-redux";
 import Image from 'next/future/image'
 import { useRouter } from "next/router";
-// import Link from "next/link";
 
 
 const Main = () => {
@@ -27,7 +25,7 @@ const Main = () => {
   return (
     <section className="flex flex-row items-center sm:text-left justify-center md:justify-between h-screen ">
       <article className="lg:mx-32 mx-16 md:w-3/5 lg:w-3/6 flex flex-col justify-center h-2/3">
-        <h1 className="text-white lg:text-6xl text-4xl mb-6">
+        <h1 className="text-white lg:text-6xl text-4xl mb-10">
           Hello, I&apos;m Sasha.
           <br />
           Professional
@@ -35,7 +33,7 @@ const Main = () => {
           Photographer
         </h1>
         <p className="text-white lg:text-4xl text-2xl font-extralight mb-10">
-          Lorem ipsum
+          "Taking photos is like rutine for me,<br/> a nice one rutine"
         </p>
         <button className="px-2.5 md:w-1/3 py-4 text-white border-2 border-white hover:bg-white hover:text-black duration-300 text-xl mb-6"
         onClick={() => router.push('/collections')}
@@ -43,9 +41,8 @@ const Main = () => {
           Collections
         </button>
         <section>
-          <button className="mr-5"><a href=""><Image src={instagram} alt="Instagram" width={50} height={50}/></a></button>
+          <button className="mr-5"><a href="https://www.instagram.com/gamewithsasha/"><Image src={instagram} alt="Instagram" width={50} height={50}/></a></button>
           <button className="mr-5"><a href="https://t.me/gamewithsasha" alt="Telegram"><Image src={telegram} alt="Telegram" width={50} height={50}/></a></button>
-          <button className="mr-5"><a href="" alt="Facebook"><Image src={facebook} alt="Facebook" width={50} height={50}/></a></button>
         </section>
       </article>
       <div className="overflow-hidden md:w-2/5 lg:w-3/6 md:flex hidden flex-row h-screen">
@@ -56,7 +53,6 @@ const Main = () => {
               key={photos[photoSlider].src}
               src={photos[photoSlider].src}
               className="relative duration-300 object-cover object-center w-full photoSlider"
-              // style={{ left: `${0 - photoSlider * 100}%` }}
               alt="Photo carusel"
             />}
       </div>
