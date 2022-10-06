f0805b4a940d51a49f2bc5c3086c31ac4ee1277c
meliphyra-ux <xennerit@icloud.com>
2022-10-06 17:21:14 +0300

    Changes in photo src

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
   images: {
-    domains: ['cdn.mos.cms.futurecdn.net', 'photocasa.ru']
+    domains: ['cdn.mos.cms.futurecdn.net', 'photocasa.ru', 'i.imgur.com']
   },
   experimental:{
     images:{
