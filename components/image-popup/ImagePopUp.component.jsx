713bfd8eeb7267b4b6cf25388da0b8910e92eb6c
meliphyra-ux <xennerit@icloud.com>
2023-01-01 22:34:12 +0200

    Measurements fix and correcting aspect ratio

--- components/image-popup/ImagePopUp.component.jsx
+++ components/image-popup/ImagePopUp.component.jsx
@@ -6,7 +6,7 @@ const ImagePopUp = ({ url, setPopUp, aspectRatio }) => {
       className="fixed px-5 z-10 top-0 left-0 w-screen h-screen bg-[rgb(0,0,0,0.7)] flex items-center justify-center"
       onClick={setPopUp}
     >
-      <picture onClick={(e) => e.stopPropagation()} className={`relative lg:h-[90%] ${aspectRatio === "horizontal" ? 'md:w-3/4 h-[30%] w-[90%]' : 'md:w-1/3 h-2/3 w-[90%]' }`}>
+      <picture onClick={(e) => e.stopPropagation()} className={`relative max-w-[95%]  ${aspectRatio === "horizontal" ? 'aspect-video w-[1200px]' : 'aspect-[3/4.5] w-[500px]'}`}>
         <Image
           layout="fill"
           objectPosition="center"
